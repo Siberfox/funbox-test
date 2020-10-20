616324343b1ff40415d4de95dc735c6c69736605
Andrew I <greyfoxjob@gmail.com>
2020-10-20 16:04:37 +0600

    initialize repo

---
+++ src/components/card.component.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+import "./card.styles.scss";
+
+const Card = () => {
+  return (
+    <li className="card">
+      <h2>НЯМУШКА</h2>
+    </li>
+  );
+};
+export default Card;
